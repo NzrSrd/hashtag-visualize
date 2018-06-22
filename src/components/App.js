97c41b22afaa5f7f58055666cabc74bf4b196b64
Ayham Kteash <ayham.kteash1995@gmail.com>
2018-06-22 11:25:09 +0200

    Create the vis component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-
+import Visualizer from "./Visualizer";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Visualizer />
       </div>
     );
   }
