acdb262940d28f0f746ca461b1e027a8619485e3
Ayham Kteash <ayham.kteash1995@gmail.com>
2018-06-22 12:28:23 +0200

    add the intial layout

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,13 @@ import rc from "randomcolor";
 
 import Visualizer from "./Visualizer";
 import MusicThing from "./MusicThing";
+import LeftSideBar from "./LeftSideBar";
+import Controls from "./Controls";
 
 const rcOptions = {
   luminosity: "light"
 };
+
 class App extends Component {
   constructor() {
     super();
@@ -41,10 +44,29 @@ class App extends Component {
   render() {
     const { tweet } = this.state;
     return (
-      <div className="App">
-        <Visualizer />
-        <MusicThing tweet={tweet} />
-      </div>
+      <Container fluid style={{ minHeight: "100vh" }}>
+        <Row>
+          <Col style={{ border: "1px solid red", maxHeight: "100vh" }} md={2}>
+            <LeftSideBar />
+          </Col>
+          <Col
+            style={{
+              border: "1px solid red",
+              maxHeight: "95vh",
+              height: "95vh"
+            }}
+            md={10}>
+            <Visualizer />
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ border: "1px solid red", height: "5vh" }} md={12}>
+            <Controls />
+          </Col>
+        </Row>
+
+        {/* <MusicThing tweet={tweet} /> */}
+      </Container>
     );
   }
 }
